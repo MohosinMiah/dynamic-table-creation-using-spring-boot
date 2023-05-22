3b244a1a94522c5c5b54fd84d8679e40586c09f2
mohosinmiah <hamza1610330816@gmail.com>
2023-05-22 06:31:41 +0600

    Project Setup

---
+++ src/main/java/com/dynamictable/dynamictable/DynamictableApplication.java
@@ -0,0 +1,13 @@
+package com.dynamictable.dynamictable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamictableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamictableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamictable/dynamictable/DynamictableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamictable.dynamictable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamictableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
